9a5a40fb025a1ccbee9461584edebdcac09161c1
Aree-Vanier <git@gregk.ca>
2021-05-02 01:15:43 -0400

    Added code to build from command

--- ddeploy.py
+++ ddeploy.py
@@ -1,9 +1,11 @@
 import argparse
 import sys
+import os
+import re
 
 # Register command line arguments
 parser = argparse.ArgumentParser(description="Deploy docker container to remote server using docker context. A configuration file is required ports and environment variables. Command line arguments will override configuration file.")
-parser.add_argument('path', metavar='Path', type=str, nargs='?', \
+parser.add_argument('path', metavar='Path', type=str, \
     help="Path to folder with dockerfile", default=".")
 parser.add_argument('--config', metavar='config', type=str, \
     help='Path to configuration file, defaults to ./ddeploy.yml', default='./ddeploy.yml')
@@ -15,7 +17,21 @@ parser.add_argument('--restart', metavar="flag", type=str, \
     help="Restart policy for the continer, defaults to always", default="always")
 
 # Parse the arguments
-parser.parse_args(sys.argv)
-
-args = parser.parse_args()
+args = vars(parser.parse_args())
 print(args)
+
+FOLDER_PATTERN = re.compile("([^/\\\\]*)$")
+
+# Validate inputs
+if(not os.path.isfile(args['path'])):
+    print("Configuration file not found, deploying from cli arguments.")
+if(args['name'] == None):
+    args['name'] = FOLDER_PATTERN.findall(os.getcwd())[0]
+if(args['context'] == None):
+    print("A context is required to deploy. Please provide on in a configuration or as an argument.")
+    sys.exit(1)
+args['name'] = args['name'].lower().replace(" ", "-")
+
+# Build the image
+print("Building image", args['name'], "from", args['path']+"/Dockerfile")
+os.system("docker --context "+args['context']+" build -t "+args['name']+" "+args['path'])
